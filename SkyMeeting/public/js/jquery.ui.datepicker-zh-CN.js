78e015b7b791ccbe35c1e2723097dbb29f4d823c
Yining <danislyn@gmail.com>
2013-03-16 22:48:06 +0800

    add templates

---
+++ SkyMeeting/public/js/jquery.ui.datepicker-zh-CN.js
@@ -0,0 +1,15 @@
+﻿jQuery(function($){  
+    $.datepicker.regional['zh-CN'] = {  
+        closeText: '关闭',  
+        prevText: '&#x3c;上月',  
+        nextText: '下月&#x3e;',  
+        currentText: '今天',  
+        monthNames: ['01月','02月','03月','04月','05月','06月', '07月','08月','09月','10月','11月','12月'],  
+        monthNamesShort: ['01月','02月','03月','04月','05月','06月', '07月','08月','09月','10月','11月','12月'],  
+        dayNames: ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],  
+        dayNamesShort: ['周日','周一','周二','周三','周四','周五','周六'],  
+        dayNamesMin: ['日','一','二','三','四','五','六'],  
+        dateFormat: 'yy/mm/dd', firstDay: 1,  
+        isRTL: false};  
+    $.datepicker.setDefaults($.datepicker.regional['zh-CN']);  
+});  
---
+++ SkyMeeting/public/js/uniform.default.js
@@ -0,0 +1,27 @@
+﻿$(function() {
+
+	// click event for customized checkbox
+	$("div.checker input").click(function(){
+		var target = $(this).parent("span");
+		if(target.hasClass("checked"))
+		{
+			target.removeClass("checked");
+		}
+		else
+		{
+			target.addClass("checked");
+		}
+	});
+
+	// click event for customized radio
+	$("div.radio input").click(function(){
+		$(this).parents("label.radio").siblings("label.radio").each(function(){
+			$(this).find("div.radio span").removeClass("checked");
+		});
+		var target = $(this).parent("span");
+		target.addClass("checked");
+	});
+
+
+
+});
