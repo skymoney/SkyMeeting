1e5eb0cd9710e9944240816a3b8ce7f271b4dffe
SkyMoney <CChain0615@gmail.com>
2013-03-18 13:25:16 +0800

    add Login app

---
+++ SkyMeeting/Login/views.py
@@ -0,0 +1,18 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+#just for simple page show
+def welcome(request):
+    if request.session.get("login",False):
+        return HttpResponse("You have login")
+    else:
+        return render_to_response('login.html')
+
+def login(request):
+    return HttpResponseRedirect('/meetlist')
+
+def memlist(request):
+    return render_to_response('meetingList.html')
+
