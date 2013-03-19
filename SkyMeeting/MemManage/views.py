d7a1c928c604e4c76a5dff68519e06be927ef517
cheng <cheng@cheng-HP-PC.(none)>
2013-03-19 20:43:45 +0800

    add views of Membermanage

--- SkyMeeting/MemManage/views.py
+++ SkyMeeting/MemManage/views.py
@@ -1,5 +1,27 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.template import Context
+from MemManage.models import User
+
 def member(request):
     return render_to_response('members.html')
+
+
+#get user,default get all users
+def getUser(request):
+    u_list=User.objects.all(cid_id="")  #get all user by specifying cid
+    
+    return render_to_response("",Context({"memberAll":u_list}))
+
+
+#get user by given groupid and cid
+def getUserByGroup(request):
+    
+    pass
+
+#get user by given tag id(s) and cid
+def getUserByTag(request):
+    pass
+
+
 
