624d5b8909692183a1c9eb32bd20494e2c4fd22f
SkyMoney <CChain0615@gmail.com>
2013-03-19 12:36:50 +0800

    db config to right

--- SkyMeeting/SkyMeeting/settings.py
+++ SkyMeeting/SkyMeeting/settings.py
@@ -17,8 +17,8 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '80',                      # Set to empty string for default.
+        'HOST': '192.168.100.21',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                    # Set to empty string for default.
     }
 }
 
