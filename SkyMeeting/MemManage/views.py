decf85820912698f9b86f655f1185f3b186d69cc
cheng <cheng@cheng-HP-PC.(none)>
2013-03-21 14:59:24 +0800

    ini group and tag

--- SkyMeeting/MemManage/views.py
+++ SkyMeeting/MemManage/views.py
@@ -9,13 +9,24 @@ def member(request):
     if "name" in request.GET:
         #get detail person info
         pass
-    
+    if "gid" in request.GET and "tid" in request.GET:
+        #has gid and tid
+        #urlFormat: /members/?gid=1&tid=1+2
+        pass
+    if "gid" in request.GET:
+        #only has gid
+        #urlFormat: /members/?gid=1
+        pass
+    if "tid" in request.GET:
+        #only has tig
+        #urlFormat: /members/?tid=1+2
+        pass
     #get all users
     # company_id is passed from request
     u_list=Role.objects.filter(company_id=1)
     g_list=Group.objects.filter(cid=1)
     t_list=Tag.objects.filter(cid=1)
-    return render_to_response('members.html',Context({"groupAll":g_list,}))
+    return render_to_response('members.html',Context({"groupAll":g_list,"tagAll":t_list,"memberAll":u_list}))
 
 
 #get user,default get all users
