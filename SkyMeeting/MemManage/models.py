2aed5b04c4f928bfcd2b468e9f39b3a8af95b577
cheng <cheng@cheng-HP-PC.(none)>
2013-03-21 13:13:28 +0800

    add getAllString method fot Tag and Group

--- SkyMeeting/MemManage/models.py
+++ SkyMeeting/MemManage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import simplejson as json
 
 # Create your models here.
 from Login.models import Account
@@ -8,22 +9,46 @@ class Company(models.Model):
     cname=models.CharField(max_length=100)
     clocation=models.CharField(max_length=100)
 
+class GroupManager(models.Manager):
+    def getAllString(self):
+        result=[]
+        for group in self.all():
+            group_dict=dict()
+            group_dict["gid"]=group.id
+            group_dict["gname"]=group.tname
+            group_dict["cid"]=group.cid.id
+            result.append(group_dict)
+        return json.dumps(result)
+
 class Group(models.Model):
     gname=models.CharField(max_length=50)
     cid=models.ForeignKey(Company)
     
+    objects=GroupManager()
     def toString(self):
         return "{'gid':"+str(self.id)+",'gname':"+self.gname+",'cid':"+str(self.cid)+"}"
     
     def __unicode__(self):
         return self.gname
 
+class TagManager(models.Manager):
+    def getAllString(self):
+        result=[]
+        for tag in self.all():
+            tag_dict=dict()
+            tag_dict["tid"]=tag.id
+            tag_dict["tname"]=tag.tname
+            tag_dict["cid"]=tag.cid.id
+            result.append(tag_dict)
+        return json.dumps(result)
+
 class Tag(models.Model):
     tname=models.CharField(max_length=50)
     cid=models.ForeignKey(Company)
     
+    objects=TagManager()
     def toString(self):
-        return "{'tid':"+str(self.id)+",'tname':"+self.tname+",'cid':"+str(self.cid)+"}"
+        return "{'tid':"+str(self.id)+",'tname':"+self.tname+",'cid':"+str(self.cid.id)+"}"
     
     def __unicode__(self):
         return self.tname
