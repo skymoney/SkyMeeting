45264d9dc664557a43187d0b88caecf7ee493112
Yining <danislyn@gmail.com>
2013-04-03 16:05:22 +0800

    change datepicker to datetimepicker of newMeeting.html

---
+++ SkyMeeting/public/js/jquery.ui.timepicker-zh-CN.js
@@ -0,0 +1,18 @@
+﻿jQuery(function($){  
+	$.timepicker.regional['zh-CN'] = {
+		timeOnlyTitle: '选择时间',
+		timeText: '时间',
+		hourText: '时',
+		minuteText: '分',
+		secondText: '秒',
+		millisecText: '微秒',
+		timezoneText: '时区',
+		currentText: '现在时间',
+		closeText: '完成',
+		timeFormat: 'hh:mm',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['zh-CN']);
+});  
