12d5d85fc1ee6e3c6d3c1f57d6e1df3032a1f3c5
cheng <cheng@cheng-HP-PC.(none)>
2013-04-07 09:17:11 +0800

    change db setting

--- SkyMeeting/SkyMeeting/settings.py
+++ SkyMeeting/SkyMeeting/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '192.168.100.21',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                    # Set to empty string for default.
     }
 }
