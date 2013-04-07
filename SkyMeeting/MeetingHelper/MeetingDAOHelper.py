c9867e270d42b77726332a0695f0a94319ed7ba8
cheng <cheng@cheng-HP-PC.(none)>
2013-04-07 16:16:35 +0800

    meeting method fix

--- SkyMeeting/MeetingHelper/MeetingDAOHelper.py
+++ SkyMeeting/MeetingHelper/MeetingDAOHelper.py
@@ -10,3 +10,26 @@ Description:provide ops with db and controller layer
 from MemManage.models import *
 from Meeting.models import *
 
+
+def meetings(params):
+    '''
+    get meetings given params
+    params:
+        permission: user permission
+        rid: role id to specify meeting
+        type: specified meeting type
+    '''
+    meetingSet=Meeting_Participant.objects.filter(role_id=params["rid"]).distinct()
+    meetingIdList=[]
+    #get all id set related to role
+    for meeting_single in meetingSet:
+        meetingIdList.append(meeting_single.meeting_id_id)
+    
+    meetingResult=Meeting.objects.filter(meeting_id__in=meetingIdList)
+    if "type" in params and params["type"]!="-1":
+        typeNum=params["type"]
+        meetingResult=meetingResult.filter(meeting_type=typeNum)
+    
+    #query for file attachment
+    return meetingResult
+    
