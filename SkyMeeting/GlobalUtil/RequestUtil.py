4b92b7695e0c2926875415b664a7d1af1a1c6c90
cheng <cheng@cheng-HP-PC.(none)>
2013-04-13 10:29:09 +0800

    fix multi language bug

--- SkyMeeting/GlobalUtil/RequestUtil.py
+++ SkyMeeting/GlobalUtil/RequestUtil.py
@@ -5,7 +5,7 @@ def getLangPack(request):
     langPack = dict()
     langPack["LANGUAGES"] = settings.LANGUAGES
     langPack["curLangCode"] = request.LANGUAGE_CODE
-    langPack["curAction"] = request.path
+    langPack["curAction"] = request.get_full_path
     
     for lang in settings.LANGUAGES:
         if lang[0] == request.LANGUAGE_CODE:
