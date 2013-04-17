a0af8d56343ac5c4621f10078cec570cbded664e
cheng <cheng@cheng-HP-PC.(none)>
2013-04-17 19:37:54 +0800

    add File ops module

---
+++ SkyMeeting/FileUtil/BasicUtil.py
@@ -0,0 +1,23 @@
+#-*- coding:uff-8 -*-
+'''
+Created on 2013-4-17
+
+@author: cheng
+'''
+from django.http import HttpResponse
+from Meeting.models import File
+
+def downloadFile(params):
+    '''
+    download select file,currently very big file not considered
+    @param fid: id of file to be downloaded 
+    '''
+    fobj=File.objects.get(file_id=1)
+    f=open(fobj.file_path,'rb')
+    fdata=f.read()
+    f.close()
+    
+    response=HttpResponse(fdata,mimetype='application/octet-stream')
+    response["Content-Disposition"]='attachment;filename=%s'%fobj.file_name
+    
+    return response
