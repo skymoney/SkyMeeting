ccb2350a5d575de503f9ce4f4ed8c3d80169540a
LuYining <danislyn@gmail.com>
2013-04-17 10:21:56 +0800

    change addMeeting to saveMeeting

--- SkyMeeting/Meeting/views.py
+++ SkyMeeting/Meeting/views.py
@@ -66,7 +66,7 @@ def newMeeting(request):
     return render_to_response('newMeeting.html', Context(result))
 
 @login_required
-def addMeeting(request):
+def saveMeeting(request):
     #permission check
     #......
     params = dict()
@@ -82,16 +82,9 @@ def addMeeting(request):
     params["files"] = request.POST["files"]                     # file ids, "1+2+3" etc.
     
     if "mid" in request.POST:
-        params["mid"] = request.POST["mid"]
-        #update meeting
-        result = dict()
-        result["success"] = "true"
-        result["mid"] = params["mid"]
-        return HttpResponse(json.dumps(result))
-        #return HttpResponse(json.dumps(MeetingDAOHelper.addMeeting(params)))
-    else:
-        #add meeting
-        return HttpResponse(json.dumps(MeetingDAOHelper.addMeeting(params)))
+        params["mid"] = request.POST["mid"]                     # mid exists when updating
+    
+    return HttpResponse(json.dumps(MeetingDAOHelper.saveMeeting(params)))
 
 @login_required
 def editMeeting(request):
