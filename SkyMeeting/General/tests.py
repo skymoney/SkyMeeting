14beb309e816cd9a1121c3a04887a974479c75d0
cheng <cheng@cheng-HP-PC.(none)>
2013-04-17 19:37:27 +0800

    edit profile unit test done

--- SkyMeeting/General/tests.py
+++ SkyMeeting/General/tests.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -8,9 +9,15 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class GeneralTest(TestCase):
+    def testEditProfile(self):
+        params=dict()
+        params["rid"]=3
+        params["name"]="艹"
+        params["sex"]="1"
+        params["phone"]="13234"
+        params["email"]="fd@fsd.com"
+        params["idcard"]="241432534"
+        
+        from GeneralHelper.GeneralHelper import editProfile
+        print editProfile(params)
